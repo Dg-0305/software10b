4554db930ca70284c139929438f560c825ae1f48
Juliantamayo20 <julian_rodriguez2210@liceonuevoshorizontes.edu.co>
2025-07-18 16:55:04 -0500

    Add files via upload

---
+++ Ejercicios Julian Rodriguez.java
@@ -0,0 +1,66 @@
+double numero1=59742;
+        double numero2=85637;
+        double multiplicacion=(numero1*numero2);
+        double suma= (numero1+numero2);
+        double resta= (numero1-numero2);
+        double division=(numero1/numero2);
+        System.out.println("la suma entre los dos numeros es"+ "  :  "+ suma);
+        System.out.println("la resta entre los dos numeros es" + "  :  "+ resta );
+        System.out.println("la multiplicacion entre los dos numeros es"+ "  :  "+ multiplicacion);
+        System.out.println("la division entre los dos numeros es"+ "  :  "+ division);
+        
+        int numero=84;
+        int cuadrado=(numero*numero);
+        System.out.println("el cuadrado del numero es"+"  :  "+ cuadrado);
+        
+      float celsius= 41;
+          float fahrenheit=(celsius*9/5)+32;
+          System.out.println("el valor en fahrenheit es de"+"  :  " + fahrenheit+"°F");
+
+        int largodelrectangulo= 28;
+        int anchodelrectangulo= 35;
+            int perimetro= 2*(largodelrectangulo+anchodelrectangulo);
+        System.out.println("el perimetro del rectangulo es de"+ "  :  " + perimetro);
+                        
+          float radio = 33;
+        float areaCirculo = 3.1416f * radio * radio;
+        System.out.println("el area del círculo  : " + areaCirculo);
+
+        int años= 64;
+        int dias= años * 365;
+            System.out.println("la cantidad de años en dias es de"+ "  :  "+ dias);
+
+        int Numero= 18;
+        System.out.println("el doble del numero es de   :"+ Numero*2);
+            System.out.println("el triple del numero es de   :"+ Numero*3);
+
+        double kilometros= 80;
+        double metros= kilometros*1000;
+        double centimetros= kilometros*100000;
+        System.out.println("la medida en metros es de   :"+ metros);
+            System.out.println("la medida en centimetros es de   :"+ centimetros);
+
+        int lado= 10;
+        System.out.println("el area del cuadrado es de   :"+ lado*lado);
+        System.out.println("el perimetro del cuadrado es de   :"+ 4*lado);
+
+        int arista= 4;
+        int volumen= arista*arista*arista;
+        System.out.println("el volumen del cubo es de   :"+ volumen);
+
+        int radioEsfera= 40;
+        float volumenEsfera = (4/3) * 3.1416f * radioEsfera * radioEsfera * radioEsfera;
+        System.out.println("el volumen de la esfera es de"+ "  : "+ volumenEsfera);
+
+        int Primerosnumerosnaturales= 1+2+3;
+            System.out.println("la suma de los tres primeros numeros naturales es de   :"+ Primerosnumerosnaturales);
+
+        int precio=9000;
+        int descuento= 45;
+        int preciofinal= precio-precio*descuento/100;
+        System.out.println("el precio final es de"+ "  :  "+ preciofinal);
+
+        int cuenta=1500000;
+        int porcentaje=10;
+        int propina= cuenta*porcentaje/100;
+        System.out.println("la propina a dejar es de   :"+ propina);
