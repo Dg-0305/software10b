cdb410a90ea0da6d4ff5cbfd958390f20c4924a3
Miguelrueda1919 <miguel_rueda3601@liceonuevoshorizontes.edu.co>
2025-07-18 16:58:11 -0500

    Add files via upload

---
+++ Miguel Angel Rueda Arevalo 10b - 18 Codigos.java
@@ -0,0 +1,127 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+// The main method must be in a class named "Main".
+class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+String name= miguel rueda
+    
+           int edad=17
+           
+           System.out.println(miguel rueda);
+           System.out.println( 17);
+       
+           System.out.println("calcular la suma de dos numeros")
+                   System.out.println(65+23);
+
+        int a=65
+        int b=23
+
+            System.out.printLn(a + b):
+                   
+           System.out.println("restar dos numeros");
+           System.out.println(12-22);
+
+        int c=12;
+        int d=22;
+        System.out.PrintLn( c-d );
+           
+           System.out.println("multiplicar dos numeros");
+           System.out.println(98*5);
+
+        int h=98;
+        int f=5;
+        System.out.printLn( h * f);
+           
+           System.out.println("division de dos numeros");
+           System.out.println(23/1);
+
+        int g=23;
+        int j= 1; 
+        System.out.PrintLn( g/ h );
+           
+           System.out.println("calcular el cuadrado de un numero");
+           System.out.println(7);
+
+        int= s=7;
+        System.out.PrintLn( s *s );
+
+        
+           
+                  
+           System.out.println("grados celcius a fahrenheit");
+           System.out.println(23*1.4+37 );
+
+        double n=23;
+        double fahrenheit = (n* 23*1.4)+37;
+        System.out.PrintLn("Temperatura en Fahrenheit: "+ fahrenheit);
+           
+           System.out.println("calcular el perimetro de un rectangulo");
+           System.out.println( 12 + 35+ 35 + 78);
+        int lado1 =12;
+        int lado2 =35;
+        int lado3 =35;
+        int lado4 =78;
+        System.out.PrintLn("perimetros; lado 1; 12 lado 2; 35 lado 3; 35 lado 4; 78");
+        System.out.PrintLn(lado1+lado2+lado 3+lado4 );
+           
+           
+           System.out.println("calcular el area de un circulo");
+           System.out.println(3.45 * 4*6 );
+
+        double t=3.45;
+        int k= 4;
+        System.out.PrintLn(t*(k*k) );
+           
+           System.out.println("convertir años a dias");
+           System.out.println(365*2);
+
+        System.out.PrintLn(365*2);
+           
+           System.out.println("calcular el doble de un numero");
+           System.out.println(4*8);
+
+            System.out.Println(9*9);
+          
+           System.out.println("calcular el triple de un numero");
+           System.out.println(5*5*5);
+
+        System.out.PrintLn(5*5*5);
+           
+           System.out.println("conversion de kilometros a metros ");
+           System.out.println(2 * 1000);
+
+        System.out.PrintLn(2*1000);
+           
+           
+          System.out.println("conversion de metros a centimetros ");
+          System.out.println(2 * 100);
+
+        System.out.PrintLn(2*100);
+        
+          System.out.println("conversion de kilometros a centimetros");
+          System.out.println(3* 100000);
+
+        System.out.printLn(3*100000);
+          
+          System.out.println("calcular el area de un cuadrado");
+          System.out.println(19 * 13);
+
+        System.out.PrintLn(19*13);
+          
+          System.out.println("calcular el perimetro de un cuadrado");
+          System.out.println(13 + 13 + 13 + 13);
+
+        System.out.PrintLn(13+13+13+13);
+
+        System.out.PrintLn("suma de los tres primeros numeros naturales");
+        System.out.PrintLn(4+5+6);
+
+        System.out.PrintLn(4+5+6);
+          
+          System.out.println("suma de los 3 primeros numeros naturales");
+          System.out.println(1 + 2 + 3);
+    }
+}
