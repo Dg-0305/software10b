bef4061e4f606351dcfc64df4108e8851df15adc
Miguelrueda1919 <miguel_rueda3601@liceonuevoshorizontes.edu.co>
2025-07-18 17:16:05 -0500

    Add files via upload

---
+++ 18 Codigos - Miguel Angel Rueda Arevalo 10b. java.java
@@ -0,0 +1,101 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+// The main method must be in a class named "Main".
+class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        public static void main(String[] args) {
+        
+String name = "Miguel Ángel Rueda Arevalo";
+System.out.println("Me llamo: " + miguel rueda);
+
+int edad = 17;
+System.out.println("Tengo " + 17);
+
+    int A= 27;
+    int B = 22;
+    int suma = A + B;
+    System.out.println("El valor de la suma de A + B es: " + suma);
+
+    int C = 17;
+    int D = 7;
+    int resta = C - D;
+    System.out.println("El valor de la resta de C - D es: " + resta);
+
+    int E= 1;
+    int F= 12;
+    int multiplicacion = E * F;
+    System.out.println("El valor de la multiplicación de E * F es: " + multiplicacion);
+ 
+    int S= 4;
+    int Y= 2;
+    int division = S / Y;
+    System.out.println("El valor de la división de S/ Y es: " + division);
+
+    int I = 8;
+    int cuadrado = I * I;
+    System.out.println("El valor de uno número al cuadrado es: " + cuadrado);
+
+    double celsius = 14;
+    double fahrenheit = (celsius * 1.8) + 32;
+    System.out.println("25 grados Celsius en Fahrenheit es: " + fahrenheit);
+
+    int B = 8;
+    int altura = 3;
+    int perimetro = 2 * (B + alt);
+    System.out.println("El perímetro del rectángulo es: " + perimetro);
+
+    double radio = 8;
+    double area = (3.14159 *radio * radio);
+    System.out.println("El área del círculo es: " + area);
+
+    int años = 37;
+    int dias = años * 365;
+    System.out.println("37 años equivalen a " + dias + " días.");
+
+    int T = 14;
+    int doble = T * 2;
+    int triple = T * 3;
+    System.out.println("El doble de " + T + " es: " + doble);
+    System.out.println("El triple de " + j + " es: " + triple);
+
+    int km = 14;
+    int metros = km * 1000;
+    int centimetros = km * 100000;
+    System.out.println("14 km son " + metros + " metros.");
+    System.out.println("14  km son " + centimetros + " centimetros.");
+
+    int lado = 7;
+    int area1 = lado * lado;
+    int perimetro1 = lado+lado+lado+lado;
+    System.out.println("Área del cuadrado: " + area1);
+    System.out.println("Perímetro del cuadrado: " + perimetro1);
+
+    int lado1 = 3;
+    int volumen = lado1 * lado1 * lado1;
+    System.out.println("El volumen del cubo es: " + volumen);
+
+    double radio2 = 12;
+    double volumen1 = (4 / 3) * 3.14159* radio2*radio2*radio2;
+    System.out.println("El volumen de la esfera es: " + volumen1);
+
+    int R= 1;
+    int A= 2;
+    int M= 3;
+    int suma1 = R+ A+ M;
+    System.out.println("La suma de los tres primeros números naturales es: " + suma1);
+
+    int precio = 1000000;
+    int porciento = 25;
+    int descuento = precio * porciento / 100;
+    System.out.println("El descuento de $" + precio + " con " + porciento + "% es: $" + descuento);
+
+    double total = 600000;
+    double porcentaje1 = 10;
+    double propina = total * porcentaje1 / 100;
+    System.out.println("La propina de $" + total + " con " + porcentaje1 + "% es: $" + propina);
+}
+    }
+}
