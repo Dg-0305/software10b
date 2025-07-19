f75e0d77a24f4cde9c3a4d95f44260639733b3ae
byvaleg <valentina_garavito1906@liceonuevoshorizontes.edu.co>
2025-07-18 19:17:32 -0500

    Add files via upload

---
+++ Valentina Garavito.java
@@ -0,0 +1,90 @@
+String name=("valentina Garavito");
+    String edad=("14");
+    System.out.println("mi nombre es  " +  name);
+    System.out.println("mi edad es  "  +   edad);
+    int a= 12;
+    int b=65;
+    int suma=a+b;
+    System.out.println("la suma de a y b es igual a " +suma);
+    
+    int c=16;
+    int d=14;
+    int resta= c-d;
+    System.out.println("la resta de c y d es igual a " +resta);
+    
+    int e=78;
+    int f=65;
+    int multiplicar= e*f;
+    System.out.println("la multiplicacion de e y f es igual a " +multiplicar);
+    
+    int g=98;
+    int h=4;
+    int dividir= g/h;
+    System.out.println("la division de g y h es igual a " +dividir);
+    
+    int i=56;
+    int potencia= i*i;
+    System.out.println("la potencia de 56 igual a " +potencia);
+    
+    
+    
+    double celsius=12;
+    double fahren= (celsius *1.8)+32;
+    System.out.println("12 grados celcius equivalen a " +fahren +" grados fahrenheit");
+    
+    int j= 8;
+    int k= 4;
+    int perimetro= j+k+j+k;
+    System.out.println("Ekl perimetro de un rectangulo es " +perimetro);
+    
+    double radio=17;
+    double area= 3.14 *radio*radio;
+    System.out.println("El area del circulo es igual a " + area);
+    
+    int años=12;
+    int dias= años*365;
+    System.out.println("12 años son iguales a " +dias +" dias");
+    
+    int m=18;
+    int doble= m*2;
+    int triple= m*3;
+    System.out.println("El doble de m es "+doble);
+    System.out.println("El triple de m es "+triple);
+    
+        int kilometros=14;
+        int metros=(kilometros*1000);
+        int centimetros=(kilometros*100000);
+        System.out.println("14 kilometros convertidos a metros es igual a "+metros);
+        System.out.println("14 kilometros convertidos a centimetros es igual a "+centimetros);
+        
+        int base=13;
+        int altura=13;
+        int areacuadrado=base*altura;
+        int Perimetro= base+altura+base+altura;
+        System.out.println("El area de un cuadrado es igual a " +areacuadrado);
+        System.out.println("El perimetro de un cuadrado es igual a "+Perimetro);
+        
+        int arista=87;
+        int volumen= arista*arista*arista;
+        System.out.println("El volumen de un cubo es igual a "+volumen);
+        
+        double esfera=12.7;
+        double volumenesfera=(4.0/3.0*Math.PI*esfera*esfera*esfera);
+         System.out.println("El volumen de una esfera es igual a "+volumenesfera);
+         
+         int sumar=1+2+3;
+         System.out.println("La suma de los tres primeros numeros naturales es igual a "+sumar);
+         
+         
+        double precio=75000; 
+       double descuento=50.0/100;
+       double preciototal= precio-(precio*descuento);
+       System.out.println("el precio final es "+preciototal);
+       
+       double cuenta=354000;
+       double propina=40.0/100;
+       double propinafinal=(cuenta*propina);
+       System.out.println("la propina es de "+propinafinal);
+       double totalcuenta=cuenta+(cuenta*propina);
+       System.out.println("el total de la cuenta es de "+totalcuenta);
+        
