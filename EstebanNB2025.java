7c083aaca91806546239cfd10787758d4b337746
ThEBoNi1459 <esteban_martinez1474@liceonuevoshorizontes.edu.co>
2025-07-18 19:11:33 -0500

    Add files via upload

---
+++ EstebanNB2025.java
@@ -0,0 +1,115 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package estebannb2025;
+
+import java.util.Locale;
+
+/**
+ *
+ * @author Cougar
+ */
+public class EstebanNB2025 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String Name= "esteban";
+int edad= 15;       
+int suma1= 80;
+double radio=2.5;
+long años =40;
+long dias= 365;
+long centimetros= 100000;
+long metros= 1000;
+byte kiometros= 4;
+int base= 65;
+int altura=89;
+int preciocamiseta= 250000;
+int porcentajedeldescuento=38;
+long preciocomida=50000;
+long propinacomida=7;
+// TODO code application logic here
+                System.out.println("Imprimir tu nombre y edad");
+        System.out.println ( Name +  edad);
+  
+       System.out.println ("calcular la suma de dos numeros");
+        System.out.println ("suma");
+    System.out.println(edad +suma1);
+    
+    System.out.println ("restar dos numeros ingresados por el usuario");
+    System.out.println ("resta");
+    System.out.println (suma1-edad);
+    
+    System.out.println ("multiplicar dos numeros y mostrar el resultado");
+    System.out.println ("multiplicacion");
+            System.out.println (suma1*edad);
+            System.out.println ("dividir dos numeros y ostrar el coeficiente");
+            System.out.println ("division");
+    System.out.println (suma1/edad);
+    
+    System.out.println ("calcular el cuadrado de dos numeros");
+    System.out.println ("cuadrado");
+    System.out.println ( suma1*suma1 );
+    
+     System.out.println ("convertir grados celcius a farenheit");
+     System.out.println ("conversion grados");
+     System.out.println (9/5* suma1+32 );
+     
+     System.out.println("calcular el perimetro de un rectangulo");
+     System.out.println("suma");
+     System.out.println (suma1+suma1+edad+edad);
+     
+     System.out.println ("calcular el area de un circulo");
+     System.out.println ("es ppii por radio a la dos");
+        
+     System.out.println (3.14 * radio*radio);
+     
+     System.out.println  ("años a dias");
+     System.out.println  ("multiplicacion");
+     System.out.println (años*dias);
+     
+      System.out.println ( "el doble el trile de un numero");
+     System.out.println ("multiplicacion");
+      System.out.println  (suma1*2);
+      System.out.println (suma1*3);
+      
+      System.out.println ("kilometros a metros");
+     System.out.println ("conversion");
+     System.out.println (metros*kiometros);
+     System.out.println ("kilometros a centimetros");
+     System.out.println (kiometros*centimetros);
+     
+       System.out.println ("calcular la area y el perimetro de un cuadrado");
+       System.out.println ("area");
+       System.out.println (base*altura);
+     System.out.println(base+base+altura+altura);
+     
+      System.out.println ("calcular el volumen de un cubo");
+      System.out.println("volumen");
+      System.out.println(base*altura*6);
+     
+     System.out.println ("calcular el volumen de una esfera");
+            System.out.println("volumen");
+              System.out.println (3.14*3/4*radio*radio*radio);
+           
+              
+              System.out.println("sumar los tres primeros numeros naturales");
+System.out.println("suma");
+              System.out.println(1+2+3);
+              
+              System.out.println("calcular el descuento aplicado a una  precio y porcentaje del descuento");
+              System.out.println("porcentajes");
+              System.out.println(preciocamiseta*porcentajedeldescuento/100);
+              System.out.println("se le resta el valor de la camisa , elresultado de la opreacion anterior");
+              System.out.println(preciocamiseta-95000);
+              
+              System.out.println("calcular la propina a dejar en un restaurante total y porcentaje deseado");
+                      System.out.println("porcentaje");
+                      System.out.println(preciocomida*propinacomida/100);
+                      System.out.println("sumamos al total del precio de la comida el valor o resultando anterior");
+              System.out.println(preciocomida+3500);
+     }
+}
