ba8999560bb0cd17a692bf5d82096768efd792b7
ericks2090 <erick_marquez2543@liceonuevoshorizontes.edu.co>
2025-07-23 19:11:49 -0500

    Add files via upload

---
+++ ErickJoilenMarquezLopez.java
@@ -0,0 +1,100 @@
+  String name= "Erick Marquez";
+   
+    long edad=16;
+    double valor1=  3.9;
+    double valor2=  5.5;
+    double sumatoria= valor1+valor2;
+    double sustraccion= valor1-valor2;
+    double multiplicacion= valor1*valor2;
+    double division= valor1/valor2;
+    long cuadrado= edad*edad;
+    double gradosces1= (2.2);
+    double gradofah2= ((gradosces1) * (1.8) + 32);
+    double perimetroo= (2*valor1)+(2*valor2);
+    double areacirculo= ((3.14159)*(cuadrado));
+    byte año= 4;
+    int dias= año*365;
+    double dobledenumero=6*6;
+    double tripledenumero=4*4*4;
+    int kilometros=5*1000;
+    int centimetros=5*10000;
+    int lado1= 10*10;
+    int perimetro1= 10+10+10+10;
+    int volumen1= 8*8*8;
+    double volumen2= 7*7*7*3.14159*4/3;
+    int naturales= 1+2+3;
+    int precio=150000;
+    double descuento= 25;
+    double total= precio*descuento/100;
+    double total1= 150000-(total);
+    double cuenta= 160000;
+    double propina= 0.10;
+    double descuentoo= cuenta*propina;
+    double totalll = cuenta+ descuentoo;
+   
+           
+   
+       
+   
+    System.out.println("1. Imprimir tu nombre y edad");
+    System.out.println("Erick Marquez" +   "  16");
+   
+    System.out.println("2. calcular la suma de dos numeros");
+    System.out.println(sumatoria);
+   
+    System.out.println("3. Restar dos numeros ingresados por el usuario");
+    System.out.println(sustraccion);
+   
+    System.out.println("4. Multiplicar dos numeros y mostrar el resultado");
+    System.out.println(multiplicacion);
+   
+    System.out.println("5. Dividir dos numeros y mostrar el cociente");
+    System.out.println(division);
+   
+    System.out.println("6. Calcular el cuadrado de un numero");
+    System.out.println(cuadrado);
+   
+    System.out.println("7. Convertir grados celsius a fahrenheit");
+    System.out.println(((gradosces1) * (1.8) + 32));
+   
+    System.out.println("8. Coalcular el perimetro de un rectangulo");
+    System.out.println(perimetroo);
+   
+    System.out.println("9. Cslcular el area de un circulo");
+    System.out.println(areacirculo);
+   
+    System.out.println("10. Convertir anss a dias");
+    System.out.println(dias);
+   
+    System.out.println("11. Calcular el doble y triple de un numero ingresado");
+    System.out.println(dobledenumero);
+    System.out.println(tripledenumero);
+   
+    System.out.println("12. Convertir kilometros a metros y centimetros");
+    System.out.println("5 kilometro");
+    System.out.println(kilometros +  " metros");
+    System.out.println(centimetros +  " centimetros");
+   
+    System.out.println("13. Calcular el area y el perimetro de un cuadrado");
+    System.out.println(lado1 +  " area");
+    System.out.println(perimetro1 + " perimetro");
+   
+    System.out.println("14. Calcular el volumne de un cubo");
+    System.out.println(volumen1);
+   
+    System.out.println("15. Calcular el volumne de una esfera");
+    System.out.println(volumen2);
+   
+    System.out.println("16. Sumar los tres primeros numeros naturales");
+    System.out.println(naturales);
+   
+    System.out.println("17. Calcular el descuento aplicado a una compra");
+    System.out.println(precio);
+    System.out.println("25%");
+    System.out.println(total +  " total ");
+    System.out.println(total1 + "  total a pagar");
+   
+    System.out.println("18. Calcular la propina a dejar en un restaurante");
+    System.out.println(cuenta +  " subtotal");
+    System.out.println(descuentoo + " propina");
+    System.out.println(totalll + "total de la cuenta");
